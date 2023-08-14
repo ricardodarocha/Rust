4a8ba6f248cb831dbef412e487e2fdc01736507c
Ricardo da Rocha <ricardodarocha@outlook.com>
2023-08-14 20:21:20 -0300

    Update main.rs

--- actix-modules/src/main.rs
+++ actix-modules/src/main.rs
@@ -1,1 +1,18 @@
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let todo_db = repository::database::Database::new();
+    let app_data = web::Data::new(todo_db);
+
+    HttpServer::new(move ||
+        App::new()
+            .app_data(app_data.clone())
+            .configure(api::api::config)
+            .service(healthcheck)
+            .default_service(web::route().to(not_found))
+            .wrap(actix_web::middleware::Logger::default())
+    )
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
 
