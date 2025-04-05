3a7991aea7168a5e0f419c1cb0462fa90be5bd31
Ricardo da Rocha <ricardodarocha@outlook.com>
2025-04-04 21:38:21 -0300

    Create e.rs

---
+++ algoritmos/Capitulo6/e.rs
@@ -0,0 +1,17 @@
+fn main() {
+  let mut i = 0;
+    
+  // Usando while
+  loop {
+    if i%2 != 0 {
+      print!("{i}", );    
+    } 
+
+    if i = 20 {
+      break;
+    } 
+    
+    i+=1;
+  } 
+  
+}
