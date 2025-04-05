efe4447149b1d3b339a3045d7941ba49285f4ec6
Ricardo da Rocha <ricardodarocha@outlook.com>
2025-04-04 21:39:42 -0300

    Create f.rs

---
+++ algoritmos/Capitulo6/f.rs
@@ -0,0 +1,17 @@
+fn main() {
+  let mut i = 1;
+    
+  // Usando while
+  loop {
+    if i%4 == 0 {
+      println!("{i}", );    
+    } 
+
+    i+=1;
+    if i < 20 {
+      break;
+    } 
+    
+  } 
+  
+}
