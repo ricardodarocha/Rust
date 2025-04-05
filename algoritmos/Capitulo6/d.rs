77fdc5694133a1dcfe41c0db6d894f3d0a922e4c
Ricardo da Rocha <ricardodarocha@outlook.com>
2025-04-04 21:34:23 -0300

    Create d.rs

---
+++ algoritmos/Capitulo6/d.rs
@@ -0,0 +1,18 @@
+fn main() {
+  let mut i = 1;
+  let mut soma=0;
+    
+  // Usando while
+  loop {
+    if i%2 == 0 {
+      soma += i;      
+    }
+
+    if i = 500 {
+      break;
+    } 
+    
+    i+=1;
+  } 
+  print!("{soma}", );
+}
